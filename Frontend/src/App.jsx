1ee733c74b5c1a2a0f8579bf5109c38436327277
pratik-prakash-sharma <pratikprakashsharma@gmail.com>
2025-07-16 10:26:25 +0530

    update app.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -23,6 +23,7 @@ function App() {
   async function reviewCode() {
     const response = await axios.post('http://localhost:3000/ai/get-review', { code })
     setReview(response.data)
+    console.log(response.data)
   }
 
   return (
